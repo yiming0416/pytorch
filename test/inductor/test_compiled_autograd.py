9a6b49d4dbc01b1c03f2f9ee861ef05d58aa021e
Aleksei Nikiforov <aleksei.nikiforov@linux.ibm.com>
2024-05-21 13:09:51 +0200

    Skip tests failing on s390x CI

--- test/inductor/test_compiled_autograd.py
+++ test/inductor/test_compiled_autograd.py
@@ -22,7 +22,7 @@ from torch._dynamo import compiled_autograd, config
 from torch._dynamo.utils import counters
 from torch._inductor import config as inductor_config
 from torch._inductor.test_case import run_tests, TestCase
-from torch.testing._internal.common_utils import skipIfWindows
+from torch.testing._internal.common_utils import IS_S390X, skipIfWindows
 from torch.testing._internal.inductor_utils import HAS_CPU, HAS_CUDA
 from torch.testing._internal.logging_utils import logs_to_string
 
@@ -2176,6 +2176,7 @@ TORCH_LIBRARY(test_cudagraphs_cpu_scalar_used_in_cpp_custom_op, m) {
             not in logs.getvalue()
         )
 
+    @unittest.skipIf(IS_S390X, "doesn't work on s390x")
     def test_verbose_logs_graph(self):
         def fn():
             model = torch.nn.Sequential(
@@ -2343,6 +2344,7 @@ TORCH_LIBRARY(test_cudagraphs_cpu_scalar_used_in_cpp_custom_op, m) {
         )
 
     @skipIfWindows(msg="AssertionError: Scalars are not equal!")
+    @unittest.skipIf(IS_S390X, "doesn't work on s390x")
     def test_verbose_logs_cpp(self):
         torch._logging.set_logs(compiled_autograd_verbose=True)
 
