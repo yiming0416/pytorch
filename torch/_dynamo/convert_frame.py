c0f5f65200909abd6800107691158f94ae05b713
James Wu <jjwu@meta.com>
2024-09-09 13:06:05 -0700

    Add remote cache time saved to compilation metrics (#135490)
    Summary:
    X-link: https://github.com/pytorch/benchmark/pull/2449
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/135490
    
    Record remote cache time saved via frame_phase_timing
    
    We add to the "phase" when remote cache hits and saves us time, so that we have a 1:1 correspondence between a frame and time saved.
    
    Test Plan:
    Internally run benchmark, see that it's populated in sandbox table after previous diff lands and logger config is actualized.
    
    Show that column exists in table:
    
    https://fburl.com/scuba/logger_staging_jjwu_30582a48f1ff9cf5f4ac50a4c40af/fp2te0ff
    
    Note that an earlier version of D62105258 had the column as a string so the staging table is a bit messed up. But you can see the most recent samples have the column populates as a float.
    
    Reviewed By: aorenste
    
    Differential Revision: D62106921

--- torch/_dynamo/convert_frame.py
+++ torch/_dynamo/convert_frame.py
@@ -989,6 +989,9 @@ def _compile(
                     ]
                     - start_possibly_missed_reinplacing_opportunities
                 )
+                remote_cache_time_saved = frame_phase_timing[frame_key].get(
+                    "remote_cache_time_saved", 0
+                )
             else:
                 guard_count = None
                 shape_env_guard_count = None
@@ -1005,6 +1008,7 @@ def _compile(
                 # If compilation failed, the entire time is wasted
                 dynamo_time_before_restart = time.time() - start_time
                 possibly_missed_reinplacing_opportunities = None
+                remote_cache_time_saved = None
 
             metrics = CompilationMetrics(
                 str(compile_id),
@@ -1034,6 +1038,7 @@ def _compile(
                 dynamo_time_before_restart,
                 guarded_code is not None,
                 possibly_missed_reinplacing_opportunities,
+                remote_cache_time_saved,
             )
             record_compilation_metrics(metrics)
             torch._dynamo.callback_handler.run_end_callbacks()
